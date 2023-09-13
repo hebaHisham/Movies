ccf9eb6a98f2a57ac4d63cf1db6ef007ddc3778f
hebaHisham <heba.hisham.abd97@gmail.com>
2023-09-13 09:21:26 -0400

    a few extra chnage regarding side menu Animations

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
 
     $('.sideBarMenuTogglePart i').click(function () {
-        // $('.coloringBox').animate({left: -boxWidth},1000);
+       
 
         if ($('.SideBarPart').css('left') == "0px") {
             $('.SideBarPart').animate({ left: -sidebarPartW }, 1000);
@@ -85,7 +85,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $('.six').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease', 
                     'opacity':'1'
                 });
@@ -94,7 +94,7 @@ $(document).ready(function () {
             
             setTimeout(function () {
                 $('.five').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease', 
                     'opacity':'1'
                 });
@@ -102,7 +102,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $('.four').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease',
                     'opacity':'1'
                 });
@@ -110,7 +110,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $('.three').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease',
                     'opacity':'1'
                 });
@@ -118,7 +118,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $('.two').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease',
                     'opacity':'1'
                 });
@@ -126,7 +126,7 @@ $(document).ready(function () {
 
             setTimeout(function () {
                 $('.one').css({
-                    'transform': `translateY(${-ulHeight}px)`,
+                    'transform': `translateY(10px)`,
                     'transition': 'all 1s ease' ,
                     'opacity':'1'
                 });
